1d921bdf25eccc92d4a39c424e2448a044ab4ad3
GolamRabby45 <golamrabby1203045@gmail.com>
2022-07-23 09:42:53 +0200

    output stream in cpp

---
+++ ostream.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <string.h>
+
+/*
+    std::ostream& operator<<(std::ostream& os, const Mystring& obj){
+        os << obj.str; // if friend function 
+        // os << obj.get_str() << std::endl; // if not friend function 
+
+        return os;
+    }
+
+att:
+    --> return a reference to the ostream, so we can keep inserting 
+    --> dont return ostream by value;
+*/
+class Mystring
+{
+    friend std::ostream &operator<<(std::ostream &os, const Mystring &rhs);
+    friend std::istream &operator>>(std::istream &in, Mystring &rhs);
+
+private:
+    char *str;      // pointer to a char[] that holds a C-style string
+public:
+    Mystring();                                                         // No-args constructor
+    Mystring(const char *s);                                     // Overloaded constructor
+    Mystring(const Mystring &source);                    // Copy constructor
+    Mystring( Mystring &&source);                         // Move constructor
+    ~Mystring();                                                     // Destructor
+    
+    Mystring &operator=(const Mystring &rhs); // Copy assignment
+    Mystring &operator=(Mystring &&rhs);       // Move assignment
+    
+    void display() const;
+    
+    int get_length() const;                                       // getters
+    const char *get_str() const;
+};
+
+#include <iostream>
+#include <cstring>
+#include "Mystring.h"
+
+// No-args constructor
+Mystring::Mystring() 
+    : str{nullptr} {
+    str = new char[1];
+    *str = '\0';
+}
+
+// Overloaded constructor
+Mystring::Mystring(const char *s) 
+    : str {nullptr} {
+        if (s==nullptr) {
+            str = new char[1];
+            *str = '\0';
+        } else {
+            str = new char[strlen(s)+1];
+            strcpy(str, s);
+        }
+}
+
+// Copy constructor
+Mystring::Mystring(const Mystring &source) 
+    : str{nullptr} {
+        str = new char[strlen(source.str)+ 1];
+        strcpy(str, source.str);
+        std::cout << "Copy constructor used" << std::endl;
+
+}
+
+// Move constructor
+Mystring::Mystring( Mystring &&source) 
+    :str(source.str) {
+        source.str = nullptr;
+        std::cout << "Move constructor used" << std::endl;
+}
+
+ // Destructor
+Mystring::~Mystring() {
+    delete [] str;
+}
+
+ // Copy assignment
+Mystring &Mystring::operator=(const Mystring &rhs) {
+    std::cout << "Using copy assignment" << std::endl;
+
+    if (this == &rhs) 
+        return *this;
+    delete [] str;
+    str = new char[strlen(rhs.str) + 1];
+    strcpy(str, rhs.str);
+    return *this;
+}
+
+// Move assignment
+Mystring &Mystring::operator=( Mystring &&rhs) {
+    std::cout << "Using move assignment" << std::endl;
+    if (this == &rhs) 
+        return *this;
+    delete [] str;
+    str = rhs.str;
+    rhs.str = nullptr;
+    return *this;
+}
+
+
+// Display method
+void Mystring::display() const {
+    std::cout << str << " : " << get_length() << std::endl;
+}
+
+ // getters
+ int Mystring::get_length() const { return strlen(str); }
+ const char *Mystring::get_str() const { return str; }
+
+// overloaded insertion operator
+std::ostream &operator<<(std::ostream &os, const Mystring &rhs) {
+    os << rhs.str;
+    return os;
+}
+
+// overloaded extraction operator
+std::istream &operator>>(std::istream &in, Mystring &rhs) {
+    char *buff = new char[1000];
+    in >> buff;
+    rhs = Mystring{buff};
+    delete [] buff;
+    return in;
+}
+
+
+
+
+
+int main() {
+
+    return 0;
+}
